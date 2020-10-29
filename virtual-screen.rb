fc5a93695bc6b51dfaa8d6bb1550549ba0c3031c
Jordan Papaleo <papaleowebdev@gmail.com>
2020-10-29 16:35:32 -0700

    Creted the VS class to be used for tracking placement of content in to anopinionated grid

---
+++ virtual-screen.rb
@@ -0,0 +1,24 @@
+class VirtualScreen
+  @active_column = 1
+  @virtual_screen = nil
+
+  def initialize(total_columns, total_rows)
+    rows = total_rows.times.collect do |i|
+      i + 1
+    end
+
+    @virtual_screen = rows.reduce({}) do |screen, row_number|
+      screen[row_number] = total_columns.times.collect do |i|
+        0
+      end
+
+      screen
+    end
+
+    # Set first row to all ones so it cannot be used for card content
+    @virtual_screen[1].each_index do |i|
+      @virtual_screen[1][i] = 1
+    end
+  end
+
+end
